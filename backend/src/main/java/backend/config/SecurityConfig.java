8ddc2b96012550f023906f8a91ababb106f0691f
Guilherme\Guilherme <guilhermepllay157@gmail.com>
2026-03-15 17:38:49 -0300

    deploy configs

--- backend/src/main/java/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
                                 "/graphql",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/health"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
